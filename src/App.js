44bb42c8bdddc0ccb87ebf3345497b69f5ef6b93
brobertson3 <brobertson3@users.noreply.github.com>
2019-11-09 17:07:39 -0800

    All prliminary changes for the site
    This includes responsive changes and adding assets needed to render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/logo.svg";
+import chevronRight from "./images/icon-arrow.svg";
+// import banner from "./images/hero-mobile.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main-container">
+      <header className="logo-container">
+        <img alt="Site logo" src={logo} />
       </header>
+      <div className="hero-container">
+        {/* <img alt="Hero Banner" src={banner} /> */}
+      </div>
+      <div className="content-container">
+        <h1>
+          <span>WE'RE</span> COMING SOON
+        </h1>
+        <p>
+          Hello fellow shoppers! We’re currently building our new fashion store.
+          Add your email below to stay up-to-date with announcements and our
+          launch deals.
+        </p>
+        <form>
+          <input id="email-input" type="email" placeholder="Email Address" />
+          <button id="submit-button" type="submit">
+            <img alt="Right chevron" src={chevronRight} />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
